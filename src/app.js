fa7bc3dc55e3997ad9ef230ab692fcd8242366ee
Artjoms <artjoms.v.petrovs@gmail.com>
2021-02-02 23:35:59 +0200

    Testing GIT triggers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello, Artjoms!'));
 console.log('Successfully registered path /');
 
 app.get('/health', (req, res) => res.send('Hello World is healthy! I\'m running in ' + process.env.ENVIRONMENT_NAME));
